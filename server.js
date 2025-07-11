271f5d23517c8d1e7842ef79e0b4808ceb8e19d0
thaddeusrayd <thaddeusrayd@gmail.com>
2025-07-10 21:28:00 -0500

    adds sync hashing and comparing

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //END_ASYNC
 
 //START_SYNC
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
 
 const PORT = process.env.PORT || 3000;
